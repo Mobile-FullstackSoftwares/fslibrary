8a21af842093137411f238af0b0c18a5950dd75a
Ramdeep Singh <Ramdeep Singh>
2022-10-27 16:23:27 -0400

    Activity added with flag

--- app/src/main/java/com/fullstack/fslibrary/ui/Invoke.kt
+++ app/src/main/java/com/fullstack/fslibrary/ui/Invoke.kt
@@ -2,10 +2,9 @@ package com.fullstack.fslibrary.ui
 
 import android.content.Context
 import android.content.Intent
-import com.fullstack.fslibrary.MainActivity
 
 object Invoke {
     fun invokeNft(context: Context){
-        context.startActivity(Intent( context, MainActivity::class.java))
+        context.startActivity(Intent( context, EmptyActivity::class.java).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK))
     }
 }
