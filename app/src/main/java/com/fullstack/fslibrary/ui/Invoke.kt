c80f0f95a35c1dc0cc0df84a85aec69b77ba16c6
Ramdeep Singh <Ramdeep Singh>
2022-10-27 16:16:54 -0400

    Activity added

--- app/src/main/java/com/fullstack/fslibrary/ui/Invoke.kt
+++ app/src/main/java/com/fullstack/fslibrary/ui/Invoke.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import android.content.Intent
 import com.fullstack.fslibrary.MainActivity
 
-class Invoke {
+object Invoke {
     fun invokeNft(context: Context){
         context.startActivity(Intent( context, MainActivity::class.java))
     }
